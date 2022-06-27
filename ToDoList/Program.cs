ad41923edc63e2800410751bab38b6672a059b86
Megan McKissack <meganmckissack@gmail.com>
2022-06-27 15:01:59 -0700

    refactored if else statement to run program again until view
    Co-authored-by: Viktoria <vzubarieva@users.noreply.github.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,14 +4,14 @@ using ToDoList.Models;
 
 namespace ToDoList.Models
 {
-  public class Programm
+  public class Program
   {
     static void Main()
     {
-      List<Item> ToDos = new List<Item>{ };
+      // List<Item> ToDos = new List<Item>{ };
 
-      Item item1 = new Item(" ");
-      Item item2 = new Item(" ");
+      // Item item1 = new Item(" ");
+      // Item item2 = new Item(" ");
 
       // List<Item> result = Item.GetAll();
       // ToDos.Add(item1);
@@ -21,47 +21,26 @@ namespace ToDoList.Models
       //Console.WriteLine(item1.Description); 
 
 
-      Console.WriteLine("Would you like to add an item to your list of view your list? Add/View");
-      string userAdd = Console.ReadLine();
+      Console.WriteLine("Would you like to add an item to your list or view your list? Add/View");
+      string userAddView = Console.ReadLine();
       
-      Console.WriteLine("Would you like to view your list? Add/view");
-      string userView = Console.ReadLine();
+      // Console.WriteLine("Would you like to view your list? Add/view");
+      // string userView = Console.ReadLine();
 
-       bool add = userAdd == "add";
+      //  bool add = userAdd == "add";
 
-       bool view = userView == "view";
+      //  bool view = userView == "view";
 
-      if (add) 
-      {
-        Add();
-      } else if (view)
-      {
-        View();
-      }
+      // if (add) 
+      // {
+      //   Add();
+      // } else if (view)
+      // {
+      //   View();
+      // }
 
-      void Add()
-      {
-        Console.WriteLine("Enter an item to your to do list");
-        item1.Description = Console.ReadLine();
-        //Console.WriteLine(item1.Description); 
-        ToDos.Add(item1);
-
-        Console.WriteLine("Enter another item to your to do list");
-        item2.Description = Console.ReadLine();
-        //Console.WriteLine(item2.Description); 
-        ToDos.Add(item2);
-      }
-
-      void View()
-      {
-        foreach (Item item in ToDos)
-        {
-          Console.WriteLine("Todo List item " + item.Description);
-        } 
-      }
-
-
-      // if (userAddView == "Add") {
+      // void Add()
+      // {
       //   Console.WriteLine("Enter an item to your to do list");
       //   item1.Description = Console.ReadLine();
       //   //Console.WriteLine(item1.Description); 
@@ -71,11 +50,38 @@ namespace ToDoList.Models
       //   item2.Description = Console.ReadLine();
       //   //Console.WriteLine(item2.Description); 
       //   ToDos.Add(item2);
+      // }
 
-      // } else if (userAddView == "View") {
-        // foreach (Item list in ToDos) {
-        // Console.WriteLine(ToDos);
-        // }
+      // void View()
+      // {
+      //   foreach (Item item in ToDos)
+      //   {
+      //     Console.WriteLine("Todo List item " + item.Description);
+      //   } 
+      // }
+
+
+      if (userAddView == "Add") 
+      {
+        Console.WriteLine("Enter an item to your to do list");
+        string task = Console.ReadLine(); 
+        Item newItem = new Item(task);
+        Console.WriteLine(task + " was added to your to do list.");
+        
+
+        // Console.WriteLine("Enter another item to your to do list");
+        // item2.Description = Console.ReadLine();
+        // //Console.WriteLine(item2.Description); 
+        // ToDos.Add(item2);
+        Main();
+      } 
+      else if (userAddView == "View") 
+      {
+        List<Item> toDoList = Item.GetAll();
+        foreach (Item item in toDoList) {
+        Console.WriteLine(item.Description);
+        }
+      }
 
         //List<Item> result = Item.GetAll();
        // Console.WriteLine(result);
